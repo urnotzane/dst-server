9077431a45890144becea450fbf558ed19fd3230
kaylezang <kaylezang@tencent.com>
2022-11-29 14:00:35 +0800

    feat: 极简模组配置

---
+++ Master/modoverrides.simple.lua
@@ -0,0 +1,207 @@
+-- 极简版，尽量不影响游戏体验
+
+return {
+  -- ANCHOR epic 史诗级血量
+  ["workshop-1185229307"]={
+    ["configuration_options"]={
+      [""]="",
+      ["damage"]=true,
+      ["noepic"]=false,
+      ["nonoepic"]=false,
+      ["offset"]=0,
+      ["phase"]=true 
+    },
+    ["enabled"]=true 
+  },
+-- ANCHOR 智能小木牌
+["workshop-1595631294"]={
+  configuration_options={
+    BundleItems=false,
+    ChangeSkin=true,
+    Digornot=false,
+    DragonflyChest=false,
+    Icebox=false,
+    SaltBox=false 
+  },
+  enabled=true 
+},
+-- ANCHOR 木牌传送 (GUI)
+["workshop-2119742489"]={
+  configuration_options={ Hunger_Cost=1, Ownership=false, Sanity_Cost=1 },
+  enabled=true 
+},
+-- ANCHOR 额外的装备格子
+["workshop-2324689937"]={
+  configuration_options={
+    ["config_render"]=true,
+    ["slot_amulet"]="extrabody3",
+    ["slot_armor"]="extrabody1",
+    ["slot_backpack"]=false,
+    ["slot_band"]=false,
+    ["slot_clothing"]="extrabody2",
+    ["slot_heavy"]=false,
+    ["slot_lifevest"]=false,
+    ["slot_shell"]=false 
+  },
+  enabled=true 
+},
+-- ANCHOR Boss预测器
+["workshop-2510473186"]={
+  configuration_options={
+    antlion=true,
+    atrium_gate_cooldown=true,
+    atrium_gate_destable=true,
+    bearger=true,
+    beequeenhive=true,
+    crabking_spawner=true,
+    deerclops=true,
+    dragonfly_spawner=true,
+    fruitfly=false,
+    klaus=true,
+    malbatross=true,
+    null=0,
+    prioritytype="leasttimefirst",
+    showname=false,
+    toadstool=true 
+  },
+  enabled=true 
+},
+-- ANCHOR 猎犬预测器
+["workshop-2512988342"]={ configuration_options={  }, enabled=true },
+-- ANCHOR 全球定位
+["workshop-2528036914"]={
+  configuration_options={
+    ENABLEPINGS=true,
+    FIREOPTIONS=2,
+    OVERRIDEMODE=false,
+    SHAREMINIMAPPROGRESS=true,
+    SHOWFIREICONS=true,
+    SHOWPLAYERICONS=true,
+    SHOWPLAYERSOPTIONS=2 
+  },
+  enabled=true 
+},
+-- ANCHOR 每个背包格子数量
+["workshop-374550642"]={ configuration_options={ MAXSTACKSIZE=250 }, enabled=true },
+-- ANCHOR 血量显示
+["workshop-375859599"]={
+  configuration_options={
+    divider=5,
+    random_health_value=0,
+    random_range=0,
+    send_unknwon_prefabs=false,
+    show_type=0,
+    unknwon_prefabs=1,
+    use_blacklist=true 
+  },
+  enabled=true 
+},
+-- ANCHOR 复活
+["workshop-462434129"]={
+  configuration_options={
+    MOD_RESTART_ALLOW_KILL=true,
+    MOD_RESTART_ALLOW_RESTART=true,
+    MOD_RESTART_ALLOW_RESURRECT=true,
+    MOD_RESTART_CD_BONUS=0,
+    MOD_RESTART_CD_KILL=3,
+    MOD_RESTART_CD_MAX=0,
+    MOD_RESTART_CD_RESTART=5,
+    MOD_RESTART_CD_RESURRECT=0,
+    MOD_RESTART_FORCE_DROP_MODE=0,
+    MOD_RESTART_IGNORING_ADMIN=true,
+    MOD_RESTART_MAP_SAVE=1,
+    MOD_RESTART_RESURRECT_HEALTH=0,
+    MOD_RESTART_TRIGGER_MODE=1,
+    MOD_RESTART_WELCOME_TIPS=true,
+    MOD_RESTART_WELCOME_TIPS_TIME=6 
+  },
+  enabled=true 
+},
+-- ANCHOR Show me 鼠标悬停信息展示
+["workshop-666155465"]={
+  configuration_options={
+    chestB=-1,
+    chestG=-1,
+    chestR=-1,
+    display_hp=-1,
+    food_estimation=-1,
+    food_order=0,
+    food_style=0,
+    lang="chs",
+    show_food_units=-1,
+    show_uses=-1 
+  },
+  enabled=true 
+},
+-- ANCHOR 防卡  
+["workshop-2505341606"]={
+  configuration_options={
+    ANNOUNCE_MODE=true,
+    CLEANDAYS=15,
+    axe=3,
+    bee=10,
+    beefalo=50,
+    birdtrap=10,
+    blueprint=3,
+    bugnet=3,
+    burntground=5,
+    clean=true,
+    compass=3,
+    cutgrass=50,
+    deer=10,
+    driftwood_log=130,
+    earmuffshat=3,
+    evergreen_sparse=150,
+    feather=2,
+    festival=2,
+    firehound=10,
+    fishingrod=3,
+    flies=10,
+    flint=50,
+    frog=20,
+    glommerfuel=10,
+    guano=1,
+    hammer=3,
+    heatrock=10,
+    hound=10,
+    houndstooth=10,
+    lang=true,
+    log=50,
+    manrabbit_tail=20,
+    marsh_tree=100,
+    mosquitosack=10,
+    nitre=50,
+    oceanfishingrod=3,
+    pickaxe=3,
+    pinecone=50,
+    pitchfork=3,
+    pocket_scale=3,
+    poop=5,
+    razor=3,
+    rock_petrified_tree=150,
+    rocks=50,
+    rocky=20,
+    rottenegg=2,
+    seeds=3,
+    shovel=3,
+    silk=10,
+    skeleton_player=20,
+    slurtle=5,
+    slurtleslime=2,
+    spear=3,
+    spider=10,
+    spiderden=40,
+    spidergland=10,
+    spoiled_fish=2,
+    spoiled_food=2,
+    stack=true,
+    stinger=2,
+    torch=3,
+    trap=40,
+    twiggytree=150,
+    twigs=50,
+    winterhat=3 
+  },
+  enabled=true 
+},
+}
